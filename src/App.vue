f0bce1d7aa4c5f0564b95b66fc5aa9f154aadc0d
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-02-06 14:07:21 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import Navigation from './components/navigation/Navigation.vue';
+  import HelloWorld from './components/body/HelloWorld.vue'
+</script>
+
+<template>
+  <Navigation />
+  <HelloWorld msg="Huy website" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/navigation/Navigation.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+
+</script>
+
+<template>
+  <header class="navigation">
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+    </header>
+</template>
+
+<style scoped>
+  .navigation {
+    display: flex;
+    position: fixed;
+    width: 100%;
+    height: var(--nav-bar-height);
+    background-color: white;
+  }
+</style>
